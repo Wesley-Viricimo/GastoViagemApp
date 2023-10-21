3521dd817034109ef93cef5c74748992dd431a88
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-21 11:07:35 -0300

    Adicionando evento de click no botão calcular

--- app/src/main/java/com/example/gastoviagemapp/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagemapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.gastoviagemapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.gastoviagemapp.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding //lateinit indica que a variável será instanciada posteriormente
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root) //View Binding para conectar layout ao codigo
 
+        binding.btnCalcular.setOnClickListener(this) //Quando botão for clicado será enviada a referência do mesmo para o método onclick
 
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_calcular) {
+            calcular()
+        }
     }
 
     private fun calcular() {
-
+        Toast.makeText(this, "Testando", Toast.LENGTH_SHORT).show()
     }
+
 }
